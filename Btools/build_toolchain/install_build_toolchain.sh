97b9d4d6ba62579684b83ddbf5496f3ec6e61aab
hminid <hminid@163.com>
2020-06-07 15:55:07 +0800

    Create install_build_toolchain.sh

---
+++ Btools/build_toolchain/install_build_toolchain.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+:<<EOF
+DIR="/opt/rt-n56u/toolchain-mipsel/toolchain-3.4.x"
+DL_NAME="mipsel-linux-uclibc.tar.xz"
+DL_URL="https://github.com/hanwckf/padavan-toolchain/releases/download/v1.1/$DL_NAME"
+#
+cd /opt/rt-n56u/toolchain-mipsel
+curl -O -L $DL_URL && \
+mkdir -p $DIR && \
+tar -xvf $DL_NAME -C $DIR
+EOF
+
+cd /opt/rt-n56u/toolchain-mipsel
+./clean_toolchain
+./build_toolchain
